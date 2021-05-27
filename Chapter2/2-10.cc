e2b5a8f5b64a02d7f5a6735fcf828368c52881fb
graham picard <grahampicard@gmail.com>
2021-05-26 22:37:30 -0400

    updating chapter 2 work

---
+++ Chapter2/2-10.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int k = 0;
+    while (k != 10) {
+        // this makes cout availalbe in our local namespace.
+        using std::cout;
+        cout << "*";
+        ++k;
+    }
+    // this is standard declaration
+    std::cout << std::endl;
+    return 0;
+}
