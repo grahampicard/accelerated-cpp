7e8401c9e3b7da63a48892f1a1e35d21d36512af
graham <grahampicard@gmail.com>
2021-07-04 08:59:56 -0400

    finish 5-10

--- Chapter5/5-10.cc
+++ Chapter5/5-10.cc
@@ -2,36 +2,53 @@
 #include <string>
 #include <vector>
 
+#include "string_split.h"
+
 typedef std::vector<std::string> str_vec;
 
-// Palindromes are words that are spelled the same right to left as left to
+// Palindromes are wordds that are spelled the same right to left as left to
 // right. Write a program to find all the palindromes in a dictionary. Next
 // find the longest palindrome.
 
-int main() {
-  str_vec words{"parallax", "ecce", "loop", "mayam"};
+int main()
+{
+  str_vec dictionary;
+
+  while (std::cin)
+  {
+    std::string rawInput;
+    std::cin >> rawInput;
+    dictionary.push_back(rawInput);
+  }
+
   str_vec palindromes;
 
-  for (str_vec::iterator i = words.begin(); i != words.end(); ++i) {
+  for (str_vec::iterator i = dictionary.begin(); i != dictionary.end(); ++i)
+  {
     std::string cur_word = *i;
     bool isPalindrome = true;
-    for (str_vec::size_type k = 0; k * 2 <= cur_word.size(); ++k) {
+    for (str_vec::size_type k = 0; k * 2 <= cur_word.size(); ++k)
+    {
       str_vec::size_type l = cur_word.size() - k - 1;
 
-      if (cur_word[k] != cur_word[l]) {
+      if (cur_word[k] != cur_word[l])
+      {
         isPalindrome = false;
         break;
       }
     }
-    if (isPalindrome) {
+    if (isPalindrome)
+    {
       palindromes.push_back(cur_word);
     }
   }
 
   std::string::size_type max_len = 0;
   std::string max_palindrome;
-  for (str_vec::iterator i = palindromes.begin(); i != palindromes.end(); ++i) {
-    if (i->size() > max_len) {
+  for (str_vec::iterator i = palindromes.begin(); i != palindromes.end(); ++i)
+  {
+    if (i->size() > max_len)
+    {
       max_palindrome = *i;
       max_len = i->size();
     }
